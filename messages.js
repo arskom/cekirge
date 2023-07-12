65ff4bb772c2b0832ee87a23739f8ca1fe5ab933
OnatEngin <onatengin22@gmail.com>
2023-07-12 14:39:56 +0300

    message data converter for sqlite3 added

--- messages.js
+++ messages.js
@@ -1,6 +1,42 @@
-const add_message_txn = require ('./db.js');
+function get_message_info (uuid, message) {
 
-function get_message_info (uuid, is_reply, folder, read, localState, mimeID, wdate, files, sender, recipients, headers) {
-    
-    add_message_txn(uuid, is_reply, )
 }
+
+const convertedForMain = {
+    uuid: " ",
+    in_reply_to: " ",
+    folder: "",
+    read: 0,
+    localState: "[{}]", //sabit
+    mimeID: " ",
+    mimeIRT: " ",
+    lastUpdate: " ",
+    wdate: " ",
+    tzoffset: 0,
+    files: " ",
+    body_blob: " ",
+    size: 0,
+    sender: " ",
+    recipients: " ",    //sabit
+    subject: null,
+    head: " ",
+    type: 2,    //sabit
+    body_type: " ",
+    spam_score: 0,  //sabit
+    preview: " ",
+};
+
+const convertedForMbody = {
+    uuid: "",
+    type: 2,
+    data: "",
+    enc: "UTF-8" //sabit
+};
+
+const convertedForContent = {
+    partytype: 2, //sabit
+    parid: "",
+    parsudid: 0,
+    blob_id: "", //BLOB
+    shaft512: "", //BLOB
+};

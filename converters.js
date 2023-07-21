ab93dbde7075efd0bb0c7604771a21bb0a34bb55
Burak Arslan <burak.arslan@arskom.com.tr>
2023-07-21 16:41:05 +0300

    simplified blob id generator

--- converters.js
+++ converters.js
@@ -37,17 +37,15 @@ function insertCharacterAtIndex(text) {
 }
 
 function createRegex () {
-    const patternRegex = /^[A-V0-9]{16,}$/;
     const characters = 'ABCDEFGHIJKLMNOPQRSTUV0123456789';
+
     let randomString = '';
 
-    while (!patternRegex.test(randomString)) {
-        randomString = '';
-        for (let i = 0; i < 16; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            randomString += characters.charAt(randomIndex);
-        }
+    for (let i = 0; i < 16; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
     }
+
     return randomString;
 }
 
